1b4fe09fb584a76c5be200b87996d7dbf09aefa1
igiyazov <46863152+igiyazov@users.noreply.github.com>
2024-09-25 19:14:04 +0300

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -6,7 +6,7 @@ MAX_RECV_DATA_SIZE = 100
 BINARY_CHUNK_SIZE = 1024
 
 DEFAULT_PORT = 3490
-DEFAULT_BINARY_PATH = './build/example_app/example_app_fota_image_encrypted.bin'
+DEFAULT_BINARY_PATH = './firmware/1.bin'
 
 
 def _main():
